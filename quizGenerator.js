407b7a3ad0bc68f18a58225915ac20b6fac5d885
Christa <christadcooke@gmail.com>
2018-11-04 13:32:41 -0800

    parsed data

--- quizGenerator.js
+++ quizGenerator.js
@@ -1,15 +1,34 @@
 const fs = require('fs')
 
-// parse csv data
+var usageArray = null;
+var questions = [];
 
-fs.readFile('usage.csv', 'utf8', function (err, data) {
-  var usageArray = data.split("\n");  //Be careful if you are in a \r\n world...
-  // Your array contains ['ID', 'D11', ... ]
-  console.log(usageArray);
-})
+// fs.readFile('usage.csv', 'utf8', function (err, data) {
+//   usageArray = data.split("\n");  
+// })
 
 fs.readFile('questions.csv', 'utf8', function (err, data) {
-  var questionsArray = data.split("\n");  //Be careful if you are in a \r\n world...
-  // Your array contains ['ID', 'D11', ... ]
-  console.log(questionsArray);
+  let questionsData = data.split("\n"); 
+  questionsData.pop();
+  questionsData.shift();
+
+  let subArr = [];
+
+  for(let i = 0; i < questionsData.length; i++){
+	subArr = questionsData[i].split(",");
+	questions[i] = subArr;
+	subArr = null;
+  }
+
+  makeQuiz(5);
 })
+
+
+
+function makeQuiz(n){ // where n is size of quiz
+	if(n < 0)
+		return;
+
+	console.log(questions[0][0]);
+
+}
